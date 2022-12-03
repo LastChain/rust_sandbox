0e4f4e8c697feea1f50b7d33ce8349e6fe1cc37b
Cihan Kurt <cihan-kurt@outlook.de>
2022-12-04 00:45:56 +0100

    update - WIP - @array-splicing

---
+++ src/arrays.rs
@@ -0,0 +1,26 @@
+// Arrays - Fixed list where elements are the same data types
+
+// before L:21 was commented
+use std::mem;
+
+pub fn run() {
+  // let num: [i32;5] = [0,1,2,3,4];
+  let mut num: [i32;5] = [0,1,2,3,4];
+
+  // Re-assign value
+  num[2] = 20;
+
+  // Using Debug trait to print the whole array
+  println!("{:?}", num);
+
+
+  // Get array length
+  println!("Array Length is \"{}\".", num.len());
+
+  // Arrays are stack allocated
+  // println!("Array occupies {} bytes in total", std::mem::size_of_val(&num));
+  println!("Array occupies {} bytes in total.", mem::size_of_val(&num));
+
+  // Get single Value
+  println!("Single Values: {}, {}, {}", num[0], num[1], num[2]);
+}
--- src/tuples.rs
+++ src/tuples.rs
@@ -2,5 +2,7 @@
 // Max 12 Elements
 
 pub fn run() {
-
+    let person: (&str, &str, i8) = ("Cihan", "Berlin", 36);
+    println!("{} is from {} and is {}.", person.0, person.1, person.2);
+    
 }
